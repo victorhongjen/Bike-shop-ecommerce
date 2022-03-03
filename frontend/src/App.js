b702faa77399852dc74dd56dc1760bc9ced4fb1e
Victor Wang <hongjenwang@gmail.com>
2022-03-03 01:57:38 -0800

    Add header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className="App">
-        <h1>Welcome to BikeShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to BikeShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
