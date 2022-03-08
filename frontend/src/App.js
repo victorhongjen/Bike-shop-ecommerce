94bc0bb18e5a7218f4942e782e714d6baa48c843
Victor Wang <hongjenwang@gmail.com>
2022-03-08 13:38:01 -0800

    Add react router for the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <h1>Welcome to BikeShop</h1>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Router>
         </Container>
       </main>
       <Footer />
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
