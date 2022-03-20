9fbd88827103a7ca42b0ee73ce0389dfb466313f
Victor Wang <hongjenwang@gmail.com>
2022-03-19 17:09:23 -0700

    move middleware to separate folder and create custom error handler to res in json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -9,11 +10,22 @@ connectDB()
 
 const app = express()
 
+// impliment middleware, a function that has access to req res object
+app.use((req, res, next) => {
+    console.log(req.originalUrl)
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+//create error middleware for the routes
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
