4727ddcd58dc3b53c9abaf7cbfaa61a684ece095
Victor Wang <hongjenwang@gmail.com>
2022-03-22 10:58:58 -0700

    Add loader and message components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,7 +8,7 @@ import Product from '../models/productModel.js'
 //@access Public route 
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
-
+    
     res.json(products)
 }))
 
