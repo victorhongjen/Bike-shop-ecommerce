c9cfb22a823feacaf1ee87ef57b680b76e2ae181
Victor Wang <hongjenwang@gmail.com>
2022-03-27 13:24:53 -0700

    Add add to cart screen with quantity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,20 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <main className="py-3">
-        <Container> 
-            <Routes>
-              <Route path="/" element={<HomeScreen />} />
-              <Route path="/product/:id" element={<ProductScreen />} />
-            </Routes>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id" element={<CartScreen />} />
+            <Route path="/cart/" element={<CartScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
