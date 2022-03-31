b3cdfb7c78924a8d1d86623a56429955e2941249
Victor Wang <hongjenwang@gmail.com>
2022-03-30 18:26:41 -0700

    generate json web token and authenticate

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 //@desc Auth user and get token
 //@route POST /api/users/login
@@ -9,13 +10,13 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email })
 
-    if(user && (await user.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
-            _id: user._id,
-            name: user.name, 
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: null
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
         })
     } else {
         res.status(401)
@@ -23,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser }
+//@desc     Get user profiel
+//@route    GET /api/users/profile
+//@access   Private route
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+export { authUser, getUserProfile }
