2295d0b41acd7ed685274551b0afbc76b225deae
Victor Wang <hongjenwang@gmail.com>
2022-03-31 23:34:04 -0700

    Add user login screen and functionality

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        //send in the headers, content type of application/json. Also token for protected routes
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        //make request
+        const { data } = await axios.post('/api/users/login', { email, password }, config)
+
+        //dispatch user login success
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        })
+
+        //set user to local storage
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+        type: USER_LOGIN_FAIL,
+        payload:
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+        })
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,18 +5,23 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 // put all reducers in the combineReducers function
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
