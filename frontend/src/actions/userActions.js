10635ed5531ffc75e5180131e9973901b93448b9
Victor Wang <hongjenwang@gmail.com>
2022-04-11 15:38:50 -0700

    add delete user functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -18,6 +18,9 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -182,7 +185,6 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     })
 
     localStorage.setItem('userInfo', JSON.stringify(data))
-
   } catch (error) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
@@ -225,10 +227,50 @@ export const listUsers = () => async (dispatch, getState) => {
     // })
 
     // localStorage.setItem('userInfo', JSON.stringify(data))
-
   } catch (error) {
     dispatch({
       type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const deleteUser = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DELETE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    //send in the headers, content type of application/json. Also token for protected routes
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    //make request
+    const { data } = await axios.delete(`/api/users/${id}`, config)
+
+    //dispatch user login success
+    dispatch({
+      type: USER_DELETE_SUCCESS,
+    })
+    // dispatch({
+    //   type: USER_LOGIN_SUCCESS,
+    //   payload: data,
+    // })
+
+    // localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_DELETE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
